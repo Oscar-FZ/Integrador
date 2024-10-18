6cc7915ce24c4f4bf8e6b99c2ee3393871a62af3
Oscar-FZ <100852154+Oscar-FZ@users.noreply.github.com>
2024-10-17 22:33:19 -0600

    Add files via upload

--- test_wifi.py
+++ test_wifi.py
@@ -1,58 +1,35 @@
-#import socket
-#import struct
-#import time
-#
-#envio_str = 'Hola Mun2!'
-#
-##TCP_IP = '192.168.1.40'
-#TCP_IP = '172.21.255.137'
-#TCP_PORT = 10000
-#
-#while(envio_str != 'exit'):
-#    envio_str = str(input("Escriba el mensaje que quiera mandar: "))
-#    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-#    s.connect((TCP_IP, TCP_PORT))
-#    s.send(envio_str.encode())
-#    time.sleep(1)
-#    s.close()
+import socket
+import struct
+
+#TEC - '172.21.255.137'
+#CASITA - '192.168.1.34'
 
 
 
 def init ():
-    TCP_IP = '172.21.255.137'
+    TCP_IP = '192.168.1.34'
     TCP_PORT = 10000
-    return TCP_IP, TCP_PORT
-
-def send_message(string, TCP_IP, TCP_PORT):
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.connect((TCP_IP, TCP_PORT))
+    return s
+
+def send_message(string, s):
     s.send(string.encode())
-    #time.sleep(1)
-    s.close()
+
+
+def main():
+    s = init()
+    while True:
+        string = str(input("Digite la opcion: "))
+        if  (string == "exit"):
+            s.close()
+            break
+        else:
+            send_message(string, s)
 
 
 
-
-
-######################################################################
-#3C:6A:A7:3D:86:95
-#server = socket.socket(socket.AF_BLUETOOTH, socket.SOCK_STREAM, socket.BTPROTO_RFCOMM)
-#server.bind(("3C:6A:A7:3D:86:95", 4))
-#server.listen(1)
-
-#client, addr = server.accept()
-
-#try:
-#    while True:
-#        data = client.recv(1024)
-#        if no data:
-#            break
-#        print(f"Message: {data.decode('utf-8')}")
-#        message = input("Enter message: ")
-#        client.send(message.encode(utf-8))
-#
-#except OSError as e:
-#    pass
-#
-#client.close()
-#server.close()
+#MAIN
+main()
+            
+        
